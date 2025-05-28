f03048f2976843e1c9759176fdc686dcd2ffdae1
Marije Vellinga <vellingamarije@gmail.com>
2025-05-28 20:18:41 +0200

    mobile nav

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,10 +1,32 @@
 
-    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-    function myFunction() {
-    var x = document.getElementById("myLinks");
+/* Switch between showing and hiding the navigation menu links when user clicks on the hamburger menu */
+function myFunction() {
+    var x = document.getElementsByClassName("mobile-nav")[0];
+    console.log(x);
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
-    }
+}
+
+// Selecteer het HTML-element dat je wilt laten verdwijnen
+const element =document.getElementsByClassName("mobile-nav")[0];
+
+
+// Definieer een media query (bijvoorbeeld voor max 768px breed)
+const mediaQuery = window.matchMedia('(max-width: 650px)');
+
+// Functie om te bepalen of het element zichtbaar moet zijn
+function checkScreenSize(e) {
+  if (!e.matches) {
+    // Scherm kleiner of gelijk aan 768px: element verbergen
+    element.style.display = 'none';
+  }
+}
+
+// Voer de check meteen uit bij laden
+checkScreenSize(mediaQuery);
+
+// Luister naar veranderingen in schermgrootte
+mediaQuery.addEventListener('change', checkScreenSize);
